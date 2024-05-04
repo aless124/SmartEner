3ea4bb482f9fd5d95bd2599614825586f323324c
aless124 <alessfara28@gmail.com>
2024-05-04 23:25:19 +0200

    fix main
    Main.py was loading the page UserConnection because of the import

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import Lib
 import VariableCommune
-import pages.UserConnection as UserConnection
 from app.back.Home import run_app
 from BDD.Creation.CreateTable import CreateTable
 from BDD.Creation.CreateDB import CreateDB
+#import pages.UserConnection as UserConnection
 
 def main():
-    CreateDB()
-    CreateTable()
     Lib.st.set_page_config(
         page_title="Home",
         page_icon=":house:",
     )
-    Lib.st.sidebar.header("Main Menu")
 
     Lib.st.write("# Welcome to the Home page! 👋")
+    CreateDB()
+    CreateTable()
     return
 
 if __name__ == "__main__":
